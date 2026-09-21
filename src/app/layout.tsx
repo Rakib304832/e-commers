4b0a7ebc17b2f454be6d393e7bbdf3b6b61cf29d
Rakib304832 <h.rakib0000000000@gmail.com>
2026-09-21 09:32:57 +0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navber from "./component/shared/Navber"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navber></Navber>
+        {children}
+        <h2 className="font-bold text-5xl bg-yellow-500 py-7 text-center">front</h2>
+      </body>
     </html>
   );
 }
